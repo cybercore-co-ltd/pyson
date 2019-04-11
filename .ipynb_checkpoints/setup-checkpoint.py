7076fefabde45aa0a4096e1a845a492ae4acddbc
Tran Hai Anh Vo <vnbot2@gmail.com>
2019-04-11 22:19:00 +0700

    Initial commit

---
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyson',
+      version='1.2',
+      description='A package for unet models',
+      url='https://github.com/vnbot2/pyson.git',
+      author='Anh Vo Tran Hai',
+      author_email='anson@cinnamon.is',
+      license='CINNAMON',
+      packages=['pyson'],
+      zip_safe=False)
---
+++ README.md
@@ -0,0 +1,11 @@
+# units
+
+# Instalation
+
+pip install git+https://github.com/vnbot2/pyson.git
+
+# Usage
+
+```bash
+    
+```

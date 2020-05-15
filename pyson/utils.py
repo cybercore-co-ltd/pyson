98c4578e3dfe48a8c2d61dcdffee1ad33f7a0293
Vo Tran Hai Anh <vnbot2@gmail.com>
2020-05-15 15:51:14 +0700

    fix bug

--- pyson/utils.py
+++ pyson/utils.py
@@ -156,32 +156,33 @@ def memoize(func):
 
 
 def make_mini_dataset(json_path, image_prefix, out_dir, n=1000):
+    print("Making mini dataset", out_dir, "num images:", n)
     os.makedirs(os.path.join(out_dir, "images"), exist_ok=True)
     os.makedirs(os.path.join(out_dir, "annotations"), exist_ok=True)
     j = read_json(json_path)
-    os.makedirs(out_dir)
-    x = j["annotations"]
-    img_ids = list(set([_["image_id"] for _ in x]))
-    img_id2path = dict([_["id"]:_ for _ in j["images"]])
+    img_ids = list(set([_["image_id"] for _ in j["annotations"]]))
+    img_id2path = {_["id"]:_ for _ in j["images"]}
     images = []
     annotations = []
+    print("make images")
 
-    for image in x["images"]:
+    for image in tqdm(j["images"]):
         if image["id"] in img_ids[:n]:
             images.append(image)
-            file_name = image["filename"]
+            file_name = image["file_name"]
             old_path = os.path.join(image_prefix, file_name)
             new_path = os.path.join(out_dir, "images", file_name)
             shutil.copy(old_path, new_path)
 
-    for annotation in x["annotations"]:
+    print("make annotations")
+    for annotation in tqdm(j["annotations"]):
         if annotation["image_id"] in img_ids[:n]:
             annotations.append(annotation)
-    x["images"] = images
-    x["annotations"] = annotations
+    j["images"] = images
+    j["annotations"] = annotations
     out_json = os.path.join(out_dir, "annotations", "mini_json.json")
     with open(out_json, "w") as f:
-        json.dump(x, f)
+        json.dump(j, f)
     print(out_json)
 
 
